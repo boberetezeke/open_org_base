16e87b6fe090ce3c3007fbd0eb89b5a7ab6a3bd8
Steve Tuckner <steve@open-orgsolutions.com>
2013-05-06 19:47:48 -0500

    Added in necessary support for using factories and authentication in tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,6 +6,8 @@ require "rails/test_help"
 require "shoulda"
 require "mocha/setup"
 
+FactoryGirl.find_definitions
+
 Rails.backtrace_cleaner.remove_silencers!
 
 # Load support files
@@ -14,4 +16,10 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 # Load fixtures from the engine
 if ActiveSupport::TestCase.method_defined?(:fixture_path=)
   ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
+end
+
+class ActiveSupport::TestCase
+  def set_current_user(user)
+    session[:user_id] = user.id
+  end
 end
