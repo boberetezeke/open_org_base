17c051e890433845723a223f3e340892404167d6
Steve Tuckner <steve@open-orgsolutions.com>
2013-05-06 19:54:11 -0500

    added use_route onto get in functional test

--- test/functional/tasks_controller_test.rb
+++ test/functional/tasks_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
+module OpenOrgBase
 class TasksControllerTest < ActionController::TestCase
-  
   setup do
     FactoryGraph.new(self).create_objects(
       :pta => {:users => [:bobby, :jane]},
@@ -12,18 +12,17 @@ class TasksControllerTest < ActionController::TestCase
   end
 
   test "should allow creating of new tasks assigned to the creator" do
-    get :new, :organization_id => @pta
+    get :new, :use_route => :open_org_base, :organization_id => @pta
     assert_response :success
     assert_equal assigns(:task).owner, @jane
   end
 
   test "should allow parameters on new but verify access" do
-    get :new, :user_id => @jane.id, :task_id => @schedule_meeting.id, :organization_id => @pta
+    get :new, :use_route => :open_org_base, :user_id => @jane.id, :task_id => @schedule_meeting.id, :organization_id => @pta
     assert_response :success
     assert_equal assigns(:task).name, "schedule_meeting"
     assert_equal assigns(:task).owner, @jane
     assert_equal assigns(:task).prototype, @schedule_meeting
   end
-
-
+end
 end
