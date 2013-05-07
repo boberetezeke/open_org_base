919133bafbdf1795393ca1b0c45333b9e8dac749
Steve Tuckner <steve@open-orgsolutions.com>
2013-05-06 19:52:03 -0500

    add in app routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,24 @@
 OpenOrgBase::Engine.routes.draw do
+  resources :task_definitions
+  resources :groups
+  resources :tasks
+  resources :roles
+  resources :organizations, :shallow => true do
+    resources :task_graph_definitions
+  end
+
+  resources :user_sessions do
+    member do
+      get 'new'
+      post 'create'
+      delete 'destroy'
+    end
+  end
+      
+  resources :users do
+    member do
+      get 'edit'
+      get 'dashboard'
+    end
+  end
 end
